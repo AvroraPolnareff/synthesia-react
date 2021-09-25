d4e106ab58a9d7883588da316ce6ae42a29f3d8f
AvroraPolnareff <AraInTheSkyWith@gmail.com>
2021-09-25 18:15:32 +0300

    add event emitter

---
+++ src/features/core/event-emitter/index.ts
@@ -0,0 +1,19 @@
+type Listener<T> = (state: T) => void
+
+class EventEmitter<T> {
+  private listeners: Listener<T>[] = []
+
+  public on(listener: Listener<T>) {
+    this.listeners.push(listener)
+  }
+
+  public remove(listener: Listener<T>) {
+    this.listeners = this.listeners.filter(subscribed => subscribed !== listener)
+  }
+
+  public emit(state: T) {
+    this.listeners.forEach(listener => listener(state))
+  }
+}
+
+export default EventEmitter
